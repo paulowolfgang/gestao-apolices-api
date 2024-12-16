64c1d8cd95f53d32c6f4c66f43b30a1eb4d9efce
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2024-12-16 15:14:05 -0300

    :tada: Primeiro commit

---
+++ src/main/java/br/dev/paulowolfgang/gestao_apolices/GestaoApolicesApplication.java
@@ -0,0 +1,13 @@
+package br.dev.paulowolfgang.gestao_apolices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoApolicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoApolicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/paulowolfgang/gestao_apolices/GestaoApolicesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.paulowolfgang.gestao_apolices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoApolicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
