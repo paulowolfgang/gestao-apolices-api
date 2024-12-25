9aa72bd066a5c1a6c984dffa7e3f2be3ee9dcf99
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2024-12-25 16:51:23 -0300

    :sparkles: feat: Implementados métodos no serviço de usuário

--- src/main/java/br/dev/paulowolfgang/gestao_apolices/repository/IUsuarioRepository.java
+++ src/main/java/br/dev/paulowolfgang/gestao_apolices/repository/IUsuarioRepository.java
@@ -3,5 +3,8 @@ package br.dev.paulowolfgang.gestao_apolices.repository;
 import br.dev.paulowolfgang.gestao_apolices.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
+import java.util.Optional;
+
+public interface IUsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
 }
--- src/main/java/br/dev/paulowolfgang/gestao_apolices/service/impl/UsuarioServiceImpl.java
+++ src/main/java/br/dev/paulowolfgang/gestao_apolices/service/impl/UsuarioServiceImpl.java
@@ -1,41 +1,56 @@
 package br.dev.paulowolfgang.gestao_apolices.service.impl;
 
 import br.dev.paulowolfgang.gestao_apolices.entity.Usuario;
+import br.dev.paulowolfgang.gestao_apolices.repository.IUsuarioRepository;
 import br.dev.paulowolfgang.gestao_apolices.service.IUsuarioService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class UsuarioServiceImpl implements IUsuarioService {
 
+    private final IUsuarioRepository iUsuarioRepository;
+
+    public UsuarioServiceImpl(IUsuarioRepository iUsuarioRepository) {
+        this.iUsuarioRepository = iUsuarioRepository;
+    }
 
     @Override
     public Usuario salvar(Usuario usuario) {
-        return null;
+        return iUsuarioRepository.save(usuario);
     }
 
     @Override
     public Optional<Usuario> buscarPorId(Long id) {
-        return Optional.empty();
+        return iUsuarioRepository.findById(id);
     }
 
     @Override
     public Optional<Usuario> buscarPorEmail(String email) {
-        return Optional.empty();
+        return iUsuarioRepository.findByEmail(email);
     }
 
     @Override
     public List<Usuario> listarTodos() {
-        return null;
+        return iUsuarioRepository.findAll();
     }
 
     @Override
     public Usuario atualizar(Long id, Usuario usuarioAtualizado) {
-        return null;
+        return iUsuarioRepository.findById(id)
+                .map(usuarioExistente -> {
+                    usuarioExistente.setNome(usuarioAtualizado.getNome());
+                    usuarioExistente.setEmail(usuarioAtualizado.getEmail());
+                    usuarioExistente.setSenha(usuarioAtualizado.getSenha());
+                    usuarioExistente.setPapel(usuarioAtualizado.getPapel());
+                    return iUsuarioRepository.save(usuarioExistente);
+                }).orElseThrow(() -> new IllegalArgumentException("Usuário não encontrado para o ID: " + id));
     }
 
     @Override
     public void remover(Long id) {
-
+        iUsuarioRepository.deleteById(id);
     }
 }
