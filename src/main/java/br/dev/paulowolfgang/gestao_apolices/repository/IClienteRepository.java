38802e75bb6a8e12c093411919321cd607b68e45
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2024-12-27 20:59:43 -0300

    :sparkles: feat: Criado repositório de clientes

---
+++ src/main/java/br/dev/paulowolfgang/gestao_apolices/repository/IClienteRepository.java
@@ -0,0 +1,9 @@
+package br.dev.paulowolfgang.gestao_apolices.repository;
+
+import br.dev.paulowolfgang.gestao_apolices.entity.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IClienteRepository extends JpaRepository<Cliente, Long> {
+}
--- src/main/java/br/dev/paulowolfgang/gestao_apolices/repository/IUsuarioRepository.java
+++ src/main/java/br/dev/paulowolfgang/gestao_apolices/repository/IUsuarioRepository.java
@@ -2,9 +2,11 @@ package br.dev.paulowolfgang.gestao_apolices.repository;
 
 import br.dev.paulowolfgang.gestao_apolices.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByEmail(String email);
 }
