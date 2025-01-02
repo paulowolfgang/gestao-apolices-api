342f2a1b5f6fa517c93647f158595d9d1c9962f2
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2025-01-02 13:59:18 -0300

    :sparkles: feat: Criado repositório de Apólices

---
+++ src/main/java/br/dev/paulowolfgang/gestao_apolices/repository/IApoliceRepository.java
@@ -0,0 +1,12 @@
+package br.dev.paulowolfgang.gestao_apolices.repository;
+
+import br.dev.paulowolfgang.gestao_apolices.entity.Apolice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface IApoliceRepository extends JpaRepository<Apolice, Long> {
+    Optional<Apolice> findByNumero(String numero);
+}
