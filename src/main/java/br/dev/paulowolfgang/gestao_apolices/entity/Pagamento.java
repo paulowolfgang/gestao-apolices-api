7e3040aee82423b6a94feed65236bdd4303064c9
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2025-01-04 10:40:54 -0300

    :sparkles: feat: Criada entidade de Pagamentos

---
+++ src/main/java/br/dev/paulowolfgang/gestao_apolices/entity/Pagamento.java
@@ -0,0 +1,126 @@
+package br.dev.paulowolfgang.gestao_apolices.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "pagamentos")
+public class Pagamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "O número de apólice deve ser informado.")
+    @ManyToOne
+    @JoinColumn(name = "apolice_id", nullable = false, foreignKey = @ForeignKey(name = "fk_apolice_id"))
+    private Apolice apolice;
+
+    @NotNull(message = "O valor deve ser informado")
+    @Column(nullable = false, precision = 15, scale = 2)
+    private BigDecimal valor;
+
+    @NotNull(message = "A data de vencimento é obrigatória.")
+    @Column(name = "data_vencimento", nullable = false)
+    private LocalDate dataVencimento;
+
+    @Column(name = "data_pagamento", nullable = false)
+    private LocalDate dataPagamento;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 50, columnDefinition = "ENUM('PAGO', 'ATRASADO') DEFAULT 'ATRASADO'")
+    private Status status = Status.ATRASADO;
+
+    public Pagamento(){}
+
+    public Pagamento(Long id, Apolice apolice, BigDecimal valor,
+                     LocalDate dataVencimento, LocalDate dataPagamento, Status status) {
+        this.id = id;
+        this.apolice = apolice;
+        this.valor = valor;
+        this.dataVencimento = dataVencimento;
+        this.dataPagamento = dataPagamento;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Apolice getApolice() {
+        return apolice;
+    }
+
+    public void setApolice(Apolice apolice) {
+        this.apolice = apolice;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getDataVencimento() {
+        return dataVencimento;
+    }
+
+    public void setDataVencimento(LocalDate dataVencimento) {
+        this.dataVencimento = dataVencimento;
+    }
+
+    public LocalDate getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(LocalDate dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public enum Status
+    {
+        PAGO, ATRASADO
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pagamento pagamento)) return false;
+        return Objects.equals(getId(), pagamento.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Pagamento{" +
+                "id=" + id +
+                ", apolice=" + apolice +
+                ", valor=" + valor +
+                ", dataVencimento=" + dataVencimento +
+                ", dataPagamento=" + dataPagamento +
+                ", status=" + status +
+                '}';
+    }
+}
