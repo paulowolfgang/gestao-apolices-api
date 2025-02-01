516b328440946ad9992736a4146bc5bd958fc507
Paulo Wolfgang <paulovictor.ps.belem@gmail.com>
2025-02-01 00:30:16 -0300

    :lock: security: Removido token CSRF

---
+++ src/main/java/br/dev/paulowolfgang/gestao_apolices/config/SecurrityConfig.java
@@ -0,0 +1,19 @@
+package br.dev.paulowolfgang.gestao_apolices.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurrityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(
+                authorizeRequests -> authorizeRequests.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
